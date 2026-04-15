cba0d3c84f870dcb3bd1fbadc54f850122f43b2b
UltralyticsAssistant <web@ultralytics.com>
2026-04-15 06:47:01 +0000

    Auto-format by https://ultralytics.com/actions

--- detect.py
+++ detect.py
@@ -1,13 +1,14 @@
 import warnings
-warnings.filterwarnings('ignore')
+
+warnings.filterwarnings("ignore")
 from ultralytics import YOLO
- 
- 
-if __name__ == '__main__':
-    model = YOLO('runs/train/plantdoc_exp8/weights/best.pt')
-    model.predict(source='images/bus.jpg',
-                  imgsz=640,
-                  project='runs/detect',
-                  name='exp',
-                  save=True,
-                )
+
+if __name__ == "__main__":
+    model = YOLO("runs/train/plantdoc_exp8/weights/best.pt")
+    model.predict(
+        source="images/bus.jpg",
+        imgsz=640,
+        project="runs/detect",
+        name="exp",
+        save=True,
+    )
--- train.py
+++ train.py
@@ -1,17 +1,17 @@
 import warnings
-warnings.filterwarnings('ignore')
+
+warnings.filterwarnings("ignore")
 from ultralytics import YOLO
 
-if __name__ == '__main__':
-    model = YOLO(r'ultralytics\cfg\models\v5\yolov5.yaml')
+if __name__ == "__main__":
+    model = YOLO(r"ultralytics\cfg\models\v5\yolov5.yaml")
     model.train(
-                data='datasets/PlantDoc/data.yaml',
-                cache=False,
-                imgsz=640,
-                epochs=10,
-                batch=16, 
-                workers=0, 
-                project='runs/train',
-                name='yolov5_exp',
-                )
-    
+        data="datasets/PlantDoc/data.yaml",
+        cache=False,
+        imgsz=640,
+        epochs=10,
+        batch=16,
+        workers=0,
+        project="runs/train",
+        name="yolov5_exp",
+    )
